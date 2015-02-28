3288262c56eafc016a5e6dada9364cd7ca654483
Thomas Coopman <thomas.coopman@gmail.com>
2015-02-28 21:14:55 +0100

    step0

---
+++ jsmal/src/step0_repl.js
@@ -0,0 +1,44 @@
+import readline from 'readline';
+
+
+function READ(input) {
+  return input;
+}
+
+
+function EVAL(string) {
+  return string;
+}
+
+
+function PRINT(string) {
+  return string;
+}
+
+
+function rep(input) {
+  const readInput = READ(input);
+  const evaluation = EVAL(readInput);
+  const output = PRINT(evaluation);
+  return output;
+}
+
+
+function main() {
+  const rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt('jsmal> ');
+  rl.prompt();
+
+  rl.on('line', line => {
+    const output = rep(line);
+    console.log(output);
+    rl.prompt();
+  });
+
+  rl.on('close', () => {
+    console.log('Exiting');
+    process.exit(0);
+  });
+}
+
+main();
